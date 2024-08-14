ffe7ba8a161580d8cbd224e4da249365c6c16847
Kadiri <Kadirisuccess10@gmail.com>
2024-08-14 17:38:33 +0100

    analytics update

--- React_E-Commerce/src/components/Navbar.jsx
+++ React_E-Commerce/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { Analytics } from '@vercel/analytics/react'
 
 const Navbar = () => {
     const state = useSelector(state => state.handleCart)
@@ -34,9 +35,10 @@ const Navbar = () => {
                     </div>
                 </div>
 
-
+                <Analytics />
             </div>
         </nav>
+        
     )
 }
 
